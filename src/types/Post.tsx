12780178cb5bc58e16fd03490ccfe77cddef1a15
Cerne <miguelbadanycerne.20231@poli.ufrj.br>
2025-05-13 21:30:51 -0300

    fix: updates post type

--- src/types/Post.tsx
+++ src/types/Post.tsx
@@ -1,11 +1,13 @@
 interface Post {
   id: number;
   title: string;
+  content: string;
   slug: string;
-  excerpt?: string;
   createdAt: string; // Or Date
+  updatedAt: string; // Or Date
   imageUrl?: string;
   views: number;
+  isPublished: boolean;
+  excerpt?: string;
 }
-
 export type { Post };
